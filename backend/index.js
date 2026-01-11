3c3547769b96dc996eaf1f9d7aa746ebdcaba813
manosh-code <manoshsarkar098@yahoo.com>
2026-01-11 23:11:44 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
 const express = require("express");
 
 
+const mainRouter = require("./routes/index");
+
+const app = express();
+
+app.use("/api/v1", mainRouter);
+//app.use("/api/v1/account", mainRouter);
+
+app.use("/api/v2", v2Router);
+
+
+
+
+//  /api/v1/user/signup
+//.  /api/v1/user/signin
+//.   /api/v1/user/ changePassword .....
+
+//.   /api/v1/account/transferMoney
+//.    /api/v1/account/balance
---
+++ backend/routes/index.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+
+const userRouter = require("./user");
+
+const router = express.Router();
+
+
+router.use("/user", userRouter);
+
+module.exports = router;
+
+// /api/v1/user
+//  /api/v1/transanction ..
