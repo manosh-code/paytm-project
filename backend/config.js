8eb5889763deb0bced3609f8cafaaa3302e80f26
manosh-code <manoshsarkar098@yahoo.com>
2026-01-13 19:34:44 +0530

    checkpoint

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+
+const JWT_SECRET  = "manoshsarkar";
+module.exports = JWT_SECRET 
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const app = express();
 
+const  cors = require("cors");
+app.use(cors());
 
 const mainRouter = require("./routes/index");
 
-const app = express();
+
+app.use(express.json());
+
 
 app.use("/api/v1", mainRouter);
 //app.use("/api/v1/account", mainRouter);
 
 app.use("/api/v2", v2Router);
+
+
+
+
+
+app.listen(3000);
+
+
+
+
+
+
 
 
 
